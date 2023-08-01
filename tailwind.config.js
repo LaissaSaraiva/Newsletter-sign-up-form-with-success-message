c393ae932a4010b66ed2a33e0546974f2b1758f5
Laissa Cunha <laissa.cunha@ed3.com.br>
2023-08-01 13:30:03 -0300

    chore: configura tokens em tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,23 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
+    screens: {
+      tablet: "960px",
+      desktop: "1440px"
+    },
+    colors: {
+      tomato: 'hsl(4, 100%, 67%)',
+      'dark-grey': 'hsl(234, 29%, 20%)',
+      'charcoal-grey': 'hsl(235, 18%, 26%)',
+      grey:'hsl(231, 7%, 60%)',
+      white: 'hsl(0, 0%, 100%)'
+    },
+    fontSize: {
+      sm: '16px',
+    },
+    fontFamily: {
+      roboto: 'Roboto, sans-serif',
+    },
     extend: {},
   },
   plugins: [],
