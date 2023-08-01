b5090e9ff24a34ffc4557f2aeb47e3c412846c86
Laissa Cunha <laissa.cunha@ed3.com.br>
2023-08-01 17:07:25 -0300

    feat: altera imgs para png

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,14 +4,7 @@ module.exports = {
   theme: {
     screens: {
       tablet: "960px",
-      desktop: "1440px"
-    },
-    colors: {
-      tomato: 'hsl(4, 100%, 67%)',
-      'dark-grey': 'hsl(234, 29%, 20%)',
-      'charcoal-grey': 'hsl(235, 18%, 26%)',
-      grey:'hsl(231, 7%, 60%)',
-      white: 'hsl(0, 0%, 100%)'
+      desktop: "1024px"
     },
     fontSize: {
       sm: '16px',
@@ -19,7 +12,15 @@ module.exports = {
     fontFamily: {
       roboto: 'Roboto, sans-serif',
     },
-    extend: {},
+    extend: {
+      colors: {
+        tomato: 'hsl(4, 100%, 67%)',
+        'dark-grey': 'hsl(234, 29%, 20%)',
+        'charcoal-grey': 'hsl(235, 18%, 26%)',
+        grey:'hsl(231, 7%, 60%)',
+        white: 'hsl(0, 0%, 100%)'
+      },
+    },
   },
   plugins: [],
 }
